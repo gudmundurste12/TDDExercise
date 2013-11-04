863308f4ba7ab8c1acbb24b77d333a9020a6f35d
gudmundurste12 <gudmundurste12@ru.is>
2013-11-04 15:43:35 +0000

    refactored  the TestAdd unit test

--- src/test/java/is/ru/TDDExercise/UnitTests.java
+++ src/test/java/is/ru/TDDExercise/UnitTests.java
@@ -8,10 +8,25 @@ public class UnitTests {
 
     @Test
     public void TestAdd() {
-        
+        TestAdd_EmptyString();
+		TestAdd_OneNumber();
+		TestAdd_TwoNumbers();
+    }
+	
+	public void TestAdd_EmptyString()
+	{
 		assertEquals(StringCalculator.Add(""), 0);
-		
+	}
+	
+	public void TestAdd_OneNumber()
+	{
 		assertEquals(StringCalculator.Add("1"), 1);
 		assertEquals(StringCalculator.Add("2"), 2);
-    }
+	}
+	
+	public void TestAdd_TwoNumbers()
+	{
+		
+	}
+	
 }
