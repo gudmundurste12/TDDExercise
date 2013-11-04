c190fdb5f57b98ce32e8340ca8f7a50773993a50
gudmundurste12 <gudmundurste12@ru.is>
2013-11-04 15:28:46 +0000

    Added a whole bunch of files

---
+++ src/main/java/is/ru/TDDExercise/StringCalculator.java
@@ -0,0 +1,8 @@
+package is.ru.TDDExercise;
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+    	
+    }
+}
---
+++ src/test/java/is/ru/TDDExercise/UnitTests.java
@@ -0,0 +1,13 @@
+package is.ru.TDDExercise;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class WorldTest {
+
+    @Test
+    public void greetResultsInHello() {
+        assertEquals(true, true);
+    }
+}
